25e32cd46f8c837065beb841daa651e7662cba63
Zeimd <jv.korkala@gmail.com>
2024-02-24 08:12:04 +0200

    Boilerplate for new project.

---
+++ floating_point/convert.h
@@ -0,0 +1,15 @@
+#include <vector>
+#include <stdio.h>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+
+#include <ceng/lib/timerlib.h>
+
+#include <ceng/datatypes/aligned-buffer.h>
+
+int main()
+{
+	return 0;
+}
+
