bd0ea5bcff1e2644fa504626a13817f47e23909b
Zeimd <jv.korkala@gmail.com>
2024-03-16 21:03:32 +0200

    Get return type deduction works now.

--- variadic_test/main.cpp
+++ variadic_test/main.cpp
@@ -4,13 +4,16 @@
 
 int main()
 {
-	TestTuple<int, double> a;
+	TestTuple<int, double,float> a;
 
 	a.item.value = 14;
+	a.item.item.value = 7.0;
 
 	printf("a.int = %i\n", a.item.value);
+	printf("a.int = %i\n", a.Get<0>());
 
-	printf("a.int = %i\n", a.Get<1>());
+	printf("a.int = %lf\n", a.Get<1>());
+	printf("a.int = %lf\n", a.Get<2>());
 
 	return 0;
 }
--- variadic_test/tuple_test.h
+++ variadic_test/tuple_test.h
@@ -22,43 +22,37 @@ public:
 
 	value_type<Ts...> item;
 
-	template<unsigned int N, unsigned int C, typename T, typename...Tr>
-	Get() -> T
-	{
-		using value_type<T,Tr> 
-
-		if (N == C)
-		{
-			return 
-		}
-		else
-		{
-			return Get
-		}
-	}
-
-	template <unsigned int N, unsigned int C, bool M, typename T, typename...Tr>
+	// Used for first item in tuple when N != 0
+	template <unsigned int N, unsigned int C, bool MATCH, typename CURRENT, typename...REMAINING>
 	struct get_return_type
 	{
-		using return_type = get_return_type<N, C + 1, N == C + 1, Tr>::return_type;
+		using return_type = typename get_return_type<N, C + 1, N == C, REMAINING...>::return_type;
 	};
 
-	template <unsigned int N, unsigned int C, typename T, typename...Tr>
-	struct get_return_type<N,C,true,T,Tr...>
+	// Used for first item in tuple when N==C
+	template <unsigned int N, unsigned int C, typename PREV, typename...REMAINING>
+	struct get_return_type<N, C, true, PREV, REMAINING...>
 	{
-		using return_type = T;
+		using return_type = typename PREV;
+	};
+	
+	// Used for last item in tuple when N==C
+	template <unsigned int N, unsigned int C, typename CURRENT>
+	struct get_return_type<N, C, true, CURRENT>
+	{
+		using return_type = typename CURRENT;
 	};
 
 	template<unsigned int N>
 	struct get_return_type_start
 	{
-		using return_type = get_return_type<N, 0, N == 0, Ts>::return_type;
+		using return_type = typename get_return_type<N, 1, N == 0, Ts...>::return_type;
 	};
 
 	template<unsigned int N>
-	auto Get() -> get_return_type_start<N>::return_type
+	auto Get() -> typename get_return_type_start<N>::return_type
 	{
-		return get_value<N>();
+		return N;
 	}
 };
 
